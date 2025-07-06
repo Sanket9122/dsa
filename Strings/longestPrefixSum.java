4bd442662196d9551d57351e560ba35c1ff48687
Sanket9122 <priyadarshisanket128@gmail.com>
2025-07-07 02:07:22 +0530

    commit

--- Strings/longestPrefixSum.java
+++ Strings/longestPrefixSum.java
@@ -22,3 +22,32 @@ public class longestPrefixSum {
         System.out.println(lps.longestCommonPrefix(strs));
     }
 }
+
+
+
+//2nd approach
+// class Solution {
+//     public String longestCommonPrefix(String[] strs) {
+//         if (strs == null || strs.length == 0) {
+//             return "";
+//         }
+        
+//         if (strs.length == 1) {
+//             return strs[0];
+//         }
+
+//         String firstStr = strs[0];
+
+//         for (int i = 0; i < firstStr.length(); i++) {
+//             char currentChar = firstStr.charAt(i);
+
+//             for (int j = 1; j < strs.length; j++) {
+//                 if (i == strs[j].length() || strs[j].charAt(i) != currentChar) {
+//                     return firstStr.substring(0, i);
+//                 }
+//             }
+//         }
+
+//         return firstStr;
+//     }
+// }
