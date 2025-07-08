bc24a05c13cb248686fc5a014d7ab067e0397ca7
Sanket9122 <priyadarshisanket128@gmail.com>
2025-07-09 02:58:55 +0530

    commit

--- Strings/integertoroman.java
+++ Strings/integertoroman.java
@@ -1,5 +1,25 @@
 package Strings;
-
+import java.util.*;
 public class integertoroman {
-    
+    public String intToRoman(int num) {
+        int [] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String [] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        StringBuilder romanNumeral = new StringBuilder();
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                romanNumeral.append(symbols[i]);
+                num -= values[i];
+            }
+        }
+        return romanNumeral.toString();
+    }
+    public static void main(String[] args) {
+        integertoroman itr = new integertoroman();
+        int num1 = 1994;
+        int num2 = 58;
+        int num3 = 66;
+        System.out.println(itr.intToRoman(num1)); 
+        System.out.println(itr.intToRoman(num2)); 
+        System.out.println(itr.intToRoman(num3));
+    }
 }
--- Strings/romantointeger.java
+++ Strings/romantointeger.java
@@ -1,5 +1,29 @@
 package Strings;
-
+import java.util.*;
 public class romantointeger {
-    
+    public int romanToInteger(String s){
+        HashMap<Character,Integer> romanMap = new HashMap<>();
+        romanMap.put('I', 1);
+        romanMap.put('V', 5);
+        romanMap.put('X', 10);
+        romanMap.put('L', 50);
+        romanMap.put('C', 100);
+        romanMap.put('D', 500);
+        romanMap.put('M', 1000);
+        int Int_value = 0 ;
+        for(int i = 0;i < s.length(); i++){
+            int current_value = romanMap.get(s.charAt(i));
+            if(i < s.length() -1 && current_value < romanMap.get(s.charAt(i + 1))){
+                Int_value -= current_value; 
+            } else {
+                Int_value += current_value; 
+            }
+        }
+        return Int_value;
+    }
+    public static void main(String[] args) {
+        romantointeger rti = new romantointeger();
+        String s = "MCMXCIV";
+        System.out.println(rti.romanToInteger(s));
+    }
 }
