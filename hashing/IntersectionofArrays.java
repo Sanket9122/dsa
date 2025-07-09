9e1b3ad559fb6877aa5153400a6bf2ecb53fdd07
Sanket9122 <priyadarshisanket128@gmail.com>
2025-07-10 00:56:31 +0530

    Commit

--- hashing/IntersectionofArrays.java
+++ hashing/IntersectionofArrays.java
@@ -16,8 +16,8 @@ public class IntersectionofArrays {
         return intersectionset;
     }
     public static void main(String[] args) {
-        int []a = {1, 2, 3, 4, 5};
-        int []b = {4, 5, 6, 7, 8};
+        int []a = {1, 2, 3, 4, 5 ,9};
+        int []b = {4, 5, 6, 7, 8 ,9};
         System.out.println(intersection(a,b));
     }
 }
