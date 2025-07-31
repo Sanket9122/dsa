0ed1ff0de5b3c90d9a1c30b09c1d7d25f7ed1e92
Sanket9122 <priyadarshisanket128@gmail.com>
2025-07-31 15:08:20 +0530

    commit

---
+++ LinkedList/LinkedlistCycle.java
@@ -0,0 +1,15 @@
+package LinkedList;
+import java.util.*;
+class Node {
+    int val ; 
+    Node head ;
+    Node next;
+    public Node(int val) {
+        this.val = val ;
+        this.head = null ;
+        this.next = null ;
+    }
+}
+public class LinkedlistCycle{
+    
+}
