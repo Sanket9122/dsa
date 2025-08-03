7948d0298f8f4e8f00c15981d062290466b70ea6
Sanket9122 <priyadarshisanket128@gmail.com>
2025-08-03 20:03:12 +0530

    commit

---
+++ DP/climbingstairs.java
@@ -0,0 +1,23 @@
+package DP;
+import java.util.*;
+public class climbingstairs {
+    public int climbingstairsways(int n){
+        if(n<=2){
+            return n ;
+        }
+        int [] dp = new int [n+1];
+        dp[0]= 0 ; 
+        dp[1] = 1;
+        dp[2]= 2;
+        for(int i=3 ; i<=n;i++){
+            dp[i]=  dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+    public static void main(String[] args) {
+        climbingstairs ob = new climbingstairs();
+        Scanner sc = new Scanner(System.in);
+        int n = 3;
+        System.out.println(ob.climbingstairsways(n));
+    }
+}
--- DP/fib.java
+++ DP/fib.java
@@ -5,13 +5,15 @@ public class fib {
         if(n<=1){
             return n;
         }
-        int [] dp = new int [n+1];
-        dp[0]= 0;
-        dp[1] = 1;
-        for(int i = 2 ; i<=n ;i++){
-            dp[i] = dp[i-1] + dp[i-2];
-        }
-        return dp[n];
+        // int [] dp = new int [n+1];
+        // dp[0]= 0;
+        // dp[1] = 1;
+        // for(int i = 2 ; i<=n ;i++){
+        //     dp[i] = dp[i-1] + dp[i-2];
+        // }
+        // return dp[n];
+
+        return fibonacci(n-1) + fibonacci(n-2);
     }
     public static void main(String[] args) {
         fib f = new fib();
