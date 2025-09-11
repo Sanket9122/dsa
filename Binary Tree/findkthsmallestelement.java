78c7e563219ca3d367f4e94b815b16343a267c4e
Sanket9122 <priyadarshisanket128@gmail.com>
2025-09-12 00:57:37 +0530

    commit

---
+++ Binary Tree/findkthsmallestelement.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class findkthsmallestelement {
+    static class Node {
+        int val ; 
+        Node left ,right ; 
+        Node(int val){
+            this.val = val ;
+            this.left = null ;
+            this.right = null;
+        }
+    }
+    public Node bstkthfinder(Node root){
+        Stack<Node> st = new Stack<Node>();
+
+    }
+}
--- Binary Tree/findminelement.java
+++ Binary Tree/findminelement.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class findminelement {
     static class Node {
         int val ; 
