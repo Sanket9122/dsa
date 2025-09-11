b164ea81afb856d8bfebb421b6f4fed47b50afee
Sanket9122 <priyadarshisanket128@gmail.com>
2025-09-12 01:33:46 +0530

    commit

--- Binary Tree/findkthsmallestelement.java
+++ Binary Tree/findkthsmallestelement.java
@@ -25,6 +25,29 @@ public class findkthsmallestelement {
         }
         return -1;
     }
+    int count ;
+    int result;
+    public int bstkthfinder2(Node root2 , int k2){
+        if(root2==null){
+            return -1;
+        }
+        count = k2 ; 
+        bstfinderhelper(root2);
+        return result;
+    }
+    public int bstfinderhelper(Node root2){
+        if(root2==null){
+            return -1;
+        }
+        bstfinderhelper(root2.left);
+        count--;
+        if(count==0){
+            result = root2.val;
+            return result;
+        }
+        bstfinderhelper(root2.right);
+        return -1;
+    }
     public static void main(String[] args) {
         findkthsmallestelement obj = new findkthsmallestelement();
         Node root = new Node(10);
@@ -35,5 +58,15 @@ public class findkthsmallestelement {
         root.right.right = new Node(20);
         int k = 3;
         System.out.println(obj.bstkthfinder(root, k));
+
+        findkthsmallestelement obj2 = new findkthsmallestelement();
+        Node root2 = new Node(10);
+        root2.left = new Node(5);
+        root2.right = new Node(14);
+        root2.left.left = new Node(2);
+        root2.left.right = new Node(9);
+        root2.right.right = new Node(21);
+        int k2 = 1;
+        System.out.println(obj2.bstkthfinder2(root2, k2));
     }
 }
