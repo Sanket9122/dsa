8a08f22a3974d5d811dca3410df7fc5918a710c9
Sanket9122 <priyadarshisanket128@gmail.com>
2025-09-14 00:48:43 +0530

    commit

--- Binary Tree/findkthlargestelement.java
+++ Binary Tree/findkthlargestelement.java
@@ -26,6 +26,29 @@ public class findkthlargestelement {
         }
         return -1;
     }
+
+    int count , result;
+    public int bstkthlargestfinder2(node root2, int k2){
+        if(root2==null){
+            return -1;
+        }
+        count = k2;
+        bstfinderhelper(root2);
+        return result;
+    }
+    public int bstfinderhelper(node root2){
+        if(root2==null){
+            return -1;
+        }
+        bstfinderhelper(root2.right);
+        count--;
+        if(count==0){
+            result = root2.val;
+            return result;
+        }
+        bstfinderhelper(root2.left);
+        return -1;
+    }
     public static void main(String[] args) {
         findkthlargestelement obj = new findkthlargestelement();
         node root = new node(10);
@@ -38,5 +61,14 @@ public class findkthlargestelement {
         int k1 = 7;
         System.out.println(obj.bstkthlargestfinder(root, k));
         System.out.println(obj.bstkthlargestfinder(root, k1));
+        findkthlargestelement obj2 = new findkthlargestelement();
+        node root2 = new node(10);
+        root2.left = new node(5);
+        root2.right = new node(15);
+        root2.left.left = new node(2);
+        root2.left.right = new node(7);
+        root2.right.right = new node(20);
+        int k2 = 2;
+        System.out.println(obj2.bstkthlargestfinder2(root2, k2));
     }
 }
